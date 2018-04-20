9b8641234760045ead469ce821b8b17ea683e92c
Anton Zdanov <azdanov@protonmail.com>
2018-04-20 04:32:53 +0300

    fix: add external packages

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import typescript from 'rollup-plugin-typescript2';
 export default {
   input: `src/index.ts`,
   output: [{ file: 'public/index.js', format: 'cjs', sourcemap: true }],
-  plugins: [
-    typescript(),
-  ],
+  plugins: [typescript()],
+  external: ['https', 'http', 'express', 'morgan', 'body-parser'],
 };
