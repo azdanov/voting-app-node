ca2e12b2301d79ad810ecbc0b0de76cbc570fcb5
Anton Zdanov <azdanov@protonmail.com>
2018-04-21 06:09:16 +0300

    chore: update default test

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,16 +1,16 @@
 import supertest from 'supertest';
 import app from '../src/app';
 
-describe('GET /', () => {
+describe('GET /test', () => {
   it('should return 200 OK', () => {
     return supertest(app)
-      .get('/')
+      .get('/test')
       .expect(200);
   });
 
   it('should respond with json', done => {
     return supertest(app)
-      .get('/')
+      .get('/test')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect('Content-Length', '26')
@@ -19,7 +19,7 @@ describe('GET /', () => {
 
   it('should have a message', () => {
     return supertest(app)
-      .get('/')
+      .get('/test')
       .expect(200)
       .then(response => {
         expect(response.body.message).toBe('Hello World!');
