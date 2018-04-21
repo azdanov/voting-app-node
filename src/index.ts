761b26e5970e16164f2fbf9ec800774b94a97981
Anton Zdanov <azdanov@protonmail.com>
2018-04-21 06:08:37 +0300

    feat: add dotenv and connect

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,14 @@ import http from 'http';
 import app from './app';
 import { normalizePort, onError, onListening } from './utilities';
 
+require('dotenv').config();
+import { connect } from './db';
+
 const port = normalizePort(process.env.PORT || 3000);
 
 app.set('port', port);
 
+connect(process.env.DATABASE);
 const server = http.createServer(app);
 server.listen(port);
 server.on('error', onError);
