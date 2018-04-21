df15cb82a60812ac4e3642e73c05f7159e294109
Anton Zdanov <azdanov@protonmail.com>
2018-04-21 06:30:55 +0300

    fix: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,17 @@ import typescript from 'rollup-plugin-typescript2';
 
 export default {
   input: `src/index.ts`,
-  output: [{ file: 'public/index.js', format: 'cjs', sourcemap: true }],
+  output: [{ file: 'bundle/index.js', format: 'cjs', sourcemap: true }],
   plugins: [typescript()],
-  external: ['https', 'http', 'express', 'morgan', 'body-parser'],
+  external: [
+    'https',
+    'http',
+    'express',
+    'morgan',
+    'body-parser',
+    'cors',
+    'path',
+    'winston',
+    'mongoose',
+  ],
 };
