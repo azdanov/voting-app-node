9c8d29c1c265292a603110f1751a56825c49a919
Anton Zdanov <azdanov@protonmail.com>
2018-04-21 06:09:59 +0300

    feat: add async catchErrors handler

---
+++ src/utilities/catchErrors.ts
@@ -0,0 +1,8 @@
+import { logger } from './logger';
+
+// Catch async/await errors instead of using a try block
+export const catchErrors = fn => {
+  return function(req, res, next) {
+    return fn(req, res, next).catch(next);
+  };
+};
--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,2 +1,3 @@
+import { catchErrors } from './catchErrors';
 export { logger } from './logger';
 export { normalizePort, onError, onListening } from './server';
