e653633f5df23635233d9981c30637e1a3a9f3f7
Anton Zdanov <azdanov@protonmail.com>
2018-05-14 21:39:12 +0300

    feat: use fontawesome as node package

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,28 @@
+import fontawesome from '@fortawesome/fontawesome';
+import { faGithub, faTwitter } from '@fortawesome/fontawesome-free-brands';
+import {
+  faAt,
+  faEnvelope,
+  faFileAlt,
+  faKey,
+  faSignInAlt,
+  faUserSecret,
+} from '@fortawesome/fontawesome-free-solid';
+
 import { initBurger } from './burger';
 import { initChart } from './chart';
 import { initModal } from './modal';
+
+fontawesome.library.add(
+  faTwitter,
+  faGithub,
+  faFileAlt,
+  faUserSecret,
+  faSignInAlt,
+  faAt,
+  faKey,
+  faEnvelope,
+);
 
 document.addEventListener('DOMContentLoaded', () => {
   initBurger();
